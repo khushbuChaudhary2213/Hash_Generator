02b0998f696e475a6ffc407bb3e523073adc01a3
Khushbu-Infosys <khushbuchaudhary2213@gmail.com>
2026-06-13 12:46:04 +0530

    Added PWA Functionality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Hash Calculator",
+        short_name: "HashTool",
+        theme_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png}"],
+      },
+    }),
+  ],
+});
